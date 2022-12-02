951663f248c04e97e109f41217ad7a18e81d340d
codeclanstudent <codeclanstudent@codeclanstudents-MacBook-Pro.local>
2022-12-02 11:26:15 +0000

    users app route now working, user, city and country repositories created but nothing added to them yet

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+from controllers import user_controller
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# INDEX.HTML DOES NOT EXIST YET
 
 if __name__ == "__main__":
     app.run(debug=True)
--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,18 +1,19 @@
-from flask import render_template
+from flask import Flask, render_template
 from models.user import User
 from app import app
 
 # EXAMPLES ARE USING BLUEPRINT BUT I DON'T KNOW HOW TO USE IT
 # JAMMING APP.ROUTE IN PLACE INSTEAD
 
-# RESTful CRUD Routes (not 100% sure what this means)
+# RESTful CRUD Routes 
 
 # INDEX
 # GET '/users'
 
-# @app.route("/users") 
-#   def users():
-        # return render_template('users.html')
+@app.route("/users") 
+def users():
+        user = User('James')
+        return render_template("users/index.html", users=[user])
 
 
 
