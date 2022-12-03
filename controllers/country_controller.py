1f47f5f1a06191a0fed97fc8b5c4ce4cba4282fa
James Blaney <jamesblaney93@gmail.com>
2022-12-03 19:09:39 +0000

    add countries now working

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from models.country import Country 
 from repositories import country_repository
 
 from app import app
 
-@app.route("/countries")
+@app.route('/countries')
 def countries():
     countries = country_repository.select_all()
-    return render_template("countries/index.html", countries=countries)
+    return render_template('countries/index.html', countries=countries)
+
+
+@app.route('/countries/new')
+def new_country():
+    return render_template('countries/new_country.html')
+
+
+@app.route('/countries', methods=['POST'])
+def save_country():
+    visited = False
+    form_data = request.form
+    country_name = form_data['country_name']
+    cities = form_data['cities']
+    
+    if 'visited' in form_data:  
+        visited = True
+
+    new_country = Country(country_name, cities, visited)
+    country_repository.save(new_country)
+
+    return redirect('/countries')
