e8ff7c3ee6cbfc8d1bc46b56977e11fbab9416dc
James Blaney <jamesblaney93@gmail.com>
2022-12-03 15:39:04 +0000

    m

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from controllers import user_controller
+from controllers import user_controller, country_controller, memory_controller
 
 @app.route('/')
 def home():
