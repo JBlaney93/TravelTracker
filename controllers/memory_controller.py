3e5f053c3332cf2ab7ce16bb2578d8d340c14e26
James Blaney <jamesblaney93@gmail.com>
2022-12-04 12:34:48 +0000

    add memory function added

--- controllers/memory_controller.py
+++ controllers/memory_controller.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from models.memory import Memory
-from repositories import memory_repository
+from models.user import User
+from models.country import Country
+from repositories import memory_repository, user_repository, country_repository
 
 from app import app
 
-@app.route("/memories")
+@app.route('/memories')
 def memory():
     memories = memory_repository.select_all()
-    return render_template("memories/index.html", memories=memories)
+    return render_template('memories/index.html', memories=memories)
+
+
+@app.route('/memories/new')
+def new_memory():
+    users = user_repository.select_all()
+    countries = country_repository.select_all()
+    return render_template('memories/new_memory.html', users=users, countries=countries)
+
+
+@app.route('/memories', methods=['POST'])
+def save_memory():
+    form_data = request.form
+    memory = form_data['memory']
+    user_id = form_data['user_id']
+    country_id = form_data['country_id']
+
+    user = user_repository.select_user_by_id(user_id)
+    country = country_repository.select_country_by_id(country_id)
+
+    new_memory = Memory(user, country, memory)
+    memory_repository.save(new_memory)
+
+    return redirect('/memories')
